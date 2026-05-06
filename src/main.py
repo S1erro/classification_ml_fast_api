511c61bea08d2a2085b4446e6b07c4b766d42b40
S1erro <riviev92@mail.ru>
2026-05-06 15:30:43 +0300

    post method: day 2

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 
+from .models import FeatureVectorChurn
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "ml churn service is running"}
+
+@app.post("/predict")
+async def predict(vector: FeatureVectorChurn) -> FeatureVectorChurn:
+    return vector
 
 if __name__ == "__main__":
     pass
